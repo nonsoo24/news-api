cfb47f2cd984e3c1ffadaa209b679363e5435c17
Daniel Chukwurah <dnchukwurah@sidmach.com>
2021-01-29 20:26:24 +0100

    feat: install axios

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+// const userToken = JSON.parse(localStorage.getItem('user-token'));
+// axios.defaults.baseURL = 'https://newsapi.org';
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+// axios.defaults.headers.common['Authorization'] = `Bearer ${userToken}`;
+// axios.interceptors.request.use((config) => config);
+// axios.interceptors.response.use((response) => response);
+
+Vue.config.errorHandler = function (err, vm, info) {
+  console.log(`Error: ${err.toString()}\nInfo: ${info}`)
+}
 
 Vue.config.productionTip = false;
+Vue.use(axios)
 
 new Vue({
   router,
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,39 @@
 <template>
     <div>
         <div class="card" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="">
+            <img src="../assets/logo copy.png" class="card-img-top" alt="news-img">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            news: []
+        }
+    },
+    methods: {
+        getNews() {
+            axios.get('/v2/everything?q=bitcoin&apiKey=9ea7329bd4964071a04d0f095746f02b').then(response => {
+                console.log(response.data.articles);
+                //this.news = response.data.data;
+
+            }).catch(error => {
+                console.error(error);
+            });
+
+
+        },
+    },
+    mounted() {
+       // this.getNews()
+    },
+}
+</script>
