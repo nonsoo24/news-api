612f6c66ee1151e9aa6b7d862ce1493f2aeb306f
Daniel Chukwurah <dnchukwurah@sidmach.com>
2021-01-29 23:32:41 +0100

    feat: add cards

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,39 +1,262 @@
 <template>
-    <div>
-        <div class="card" style="width: 18rem;">
-            <img src="../assets/logo copy.png" class="card-img-top" alt="news-img">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="container">
+        <div class="text-right">
+             <button class="btn btn-primary" v-if="isCard" @click="toggleCard('card')">Card view</button>
+             <button class="btn btn-primary" v-if="isCard == false" @click="toggleCard('list')">List view</button>
+        </div>
+
+        <div :class="[ isCard ? 'card-toggle' : 'list-toggle' ]">
+            <div class="card m-3" :class="[ isCard ? 'card-width' : '' ]" v-for="(article, articleIndex) in articles" :key= articleIndex>
+                <img :src= article.urlToImage :class="[ isCard ? 'card-img' : 'list-img' ]" alt="news-img">
+                <div class="card-body">
+                    <h5 class="card-title">{{article.title}}</h5>
+                    <!-- <p class="card-text">{{article.description}}</p> -->
+                    <a :href=article.url class="btn btn-primary">Read</a>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+//import axios from 'axios';
 export default {
     data() {
         return {
-            news: []
+            switchCard: 'card-toggle',
+            isCard: true,
+            articles: [{
+                    source: {
+                        "id": null,
+                        "name": "Gizmodo.com"
+                    },
+                    author: "Matt Novak",
+                    title: "India Weighs 18% Tax on Bitcoin After Legalization of Cryptocurrencies",
+                    description: "The government of India is considering an 18% tax on all bitcoin transactions, according to a new report from the Times of India. It’s not clear whether the proposed goods and services tax (GST) would apply to other cryptocurrencies like ether, the second lar…",
+                    url: "https://gizmodo.com/india-weighs-18-tax-on-bitcoin-after-legalization-of-c-1845960924",
+                    urlToImage: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/te9tpxwlizd7fmtf5ra2.jpg",
+                    publishedAt: "2020-12-29T11:50:00Z",
+                    content: "The government of India is considering an 18% tax on all bitcoin transactions, according to a new report from the Times of India. Its not clear whether the proposed goods and services tax (GST) would… [+1397 chars]"
+                },
+                {
+                    source: {
+                        "id": null,
+                        "name": "Gizmodo.com"
+                    },
+                    author: "Jody Serrano",
+                    title: "Bitcoin Keeps Moving On Up, Reaching $33,000 in Value",
+                    description: "Just weeks after it shattered its yearslong aspiration of reaching $20,000 in value, Bitcoin is at it again. On Saturday, the cryptocurrency passed $33,000 in value, according to CoinDesk, before dipping slightly throughout the day.Read more...",
+                    url: "https://gizmodo.com/bitcoin-keeps-moving-on-up-reaching-33-000-in-value-1845979632",
+                    urlToImage: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/ti4pnr0dzy4ilulignek.jpg",
+                    publishedAt: "2021-01-03T03:03:00Z",
+                    content: "Just weeks after it shattered its yearslong aspiration of reaching $20,000 in value, Bitcoin is at it again. On Saturday, the cryptocurrency passed $33,000 in value, according to CoinDesk, before dip… [+2123 chars]"
+                },
+                {
+                    source: {
+                        "id": "engadget",
+                        "name": "Engadget"
+                    },
+                    author: "Steve Dent",
+                    title: "Mt. Gox exchange users may finally get to recover some of their lost Bitcoin",
+                    description: "When Mt. Gox went bankrupt in 2014, it was the world’s largest Bitcoin exchange, holding 850,000 Bitcoins from thousands of users. Now, creditors might finally be able to get a portion of those riches back. One of Mt. Gox’s largest creditors, CoinLab, said an…",
+                    url: "https://www.engadget.com/mt-gox-bitcoin-users-recovery-153025831.html",
+                    urlToImage: "https://o.aolcdn.com/images/dims?resize=1200%2C630&crop=1200%2C630%2C0%2C0&quality=95&image_uri=https%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-images%2F2019-06%2Fcedcace0-91a9-11e9-bffe-eca270dadf9c&client=amp-blogside-v2&signature=b2965154f62f66ab9ffbed4ae4ed98155a7bb786",
+                    publishedAt: "2021-01-18T21:30:25Z",
+                    content: "When Mt. Gox went bankrupt in 2014, it was the world’s largest Bitcoin exchange, holding 850,000 Bitcoins from thousands of users. Now, creditors might finally be able to get a portion of those riche… [+626 chars]"
+                },
+                {
+                    source: {
+                        "id": "the-verge",
+                        "name": "The Verge"
+                    },
+                    author: "Elizabeth Lopatto",
+                    title: "Jack Dorsey defends Twitter’s Trump ban, then enthuses about bitcoin",
+                    description: "Twitter CEO Jack Dorsey, in a series of Tweets, explained that he felt banning US President Donald Trump’s account was the right move for the social network. “Offline harm as a result of online speech is demonstrably real, and what drives our policy and enfor…",
+                    url: "https://www.theverge.com/2021/1/13/22230028/jack-dorsey-donald-trump-twitter-ban-moderation-bitcoin-thread",
+                    urlToImage: "https://cdn.vox-cdn.com/thumbor/2pWrvN_qQZ7qWG0HEMBnYO0n8ks=/0x10:3323x1750/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22234226/1229659186.jpg",
+                    publishedAt: "2021-01-14T00:57:27Z",
+                    content: "Ban reflects Twitters failure to promote healthy conversation\r\nPhoto by Hannah McKay-Pool/Getty Images\r\nTwitter CEO Jack Dorsey, in a series of Tweets, explained that he felt banning US President Don… [+3185 chars]"
+                },
+                {
+                    source: {
+                        "id": "cnn",
+                        "name": "CNN"
+                    },
+                    author: "Shannon Liao, CNN Business",
+                    title: "Bitcoin soars past $33,000, its highest ever",
+                    description: "Bitcoin has been flirting with the $30,000 mark for weeks, and finally passed that record-setting level Saturday. The virtual currency is now valued at above $33,000, an all-time high.",
+                    url: "https://www.cnn.com/2021/01/02/investing/bitcoin-price-30000/index.html",
+                    urlToImage: "https://cdn.cnn.com/cnnnext/dam/assets/201216213407-bitcoin-file-super-tease.jpg",
+                    publishedAt: "2021-01-02T19:14:09Z",
+                    content: "New York (CNN Business)Bitcoin has been flirting with the $30,000 mark for weeks, and finally passed that record-setting level Saturday. The virtual currency is now valued at above $33,000, an all-ti… [+2108 chars]"
+                },
+                {
+                    source: {
+                        "id": "techcrunch",
+                        "name": "TechCrunch"
+                    },
+                    author: "Romain Dillet",
+                    title: "Gemini is launching a credit card with bitcoin rewards",
+                    description: "Cryptocurrency exchange company Gemini is acquiring Blockrize and announcing a new product today based on Blockrize’s work. Later this year, the company is launching a credit card that works like a regular credit card — but you earn bitcoin rewards based on y…",
+                    url: "http://techcrunch.com/2021/01/14/gemini-is-launching-a-credit-card-with-bitcoin-rewards/",
+                    urlToImage: "https://techcrunch.com/wp-content/uploads/2021/01/Tyler_Cameron-Winklevoss-2-1.jpg?w=500",
+                    publishedAt: "2021-01-14T12:00:50Z",
+                    content: "Cryptocurrency exchange company Gemini is acquiring Blockrize and announcing a new product today based on Blockrizes work. Later this year, the company is launching a credit card that works like a re… [+1479 chars]"
+                },
+                {
+                    source: {
+                        "id": "mashable",
+                        "name": "Mashable"
+                    },
+                    author: "Joseph Green",
+                    title: "Learn how to responsibly invest in Bitcoin with this bundle of courses",
+                    description: "TL;DR: The Bitcoin and Cryptocurrency Mastery Bundle is on sale for £21.11 as of Jan. 28, saving you 70% on list price.\n\nBitcoin and the blockchain are not going away anytime soon. Though market's volatility may put off some investors, there are ways to find …",
+                    url: "https://mashable.com/uk/shopping/jan-28-bitcoin-cryptocurrency-online-courses-sale/",
+                    urlToImage: "https://mondrian.mashable.com/2021%252F01%252F28%252Fa3%252F4d5390f78a6f43dfb9fafcbec5ba3bfc.8c330.jpg%252F1200x630.jpg?signature=sHhBEh8D-cE3Jy5685Ge1Bsdg8E=",
+                    publishedAt: "2021-01-28T05:10:00Z",
+                    content: "TL;DR: The Bitcoin and Cryptocurrency Mastery Bundle is on sale for £21.11 as of Jan. 28, saving you 70% on list price.\r\nBitcoin and the blockchain are not going away anytime soon. Though market's vo… [+3074 chars]"
+                },
+                {
+                    source: {
+                        "id": "mashable",
+                        "name": "Mashable"
+                    },
+                    author: "Stan Schroeder",
+                    title: "Elon Musk changes Twitter bio to #bitcoin, chaos ensues",
+                    description: "As the man himself said, it was inevitable. \nOn Friday, Tesla CEO Elon Musk changed his Twitter bio to \"#bitcoin.\" This alone perhaps wouldn't be that interesting — after all, Musk expressed his interest in Bitcoin and cryptocurrencies before — but Musk follo…",
+                    url: "https://mashable.com/article/elon-musk-bitcoin-twitter-bio/",
+                    urlToImage: "https://mondrian.mashable.com/2021%252F01%252F29%252F45%252F3c46ae3c616f4ff380595bf659b0a3e7.85f6b.jpg%252F1200x630.jpg?signature=2OKk8uyseXtIBD2VR-eDcdTb8G8=",
+                    publishedAt: "2021-01-29T10:59:39Z",
+                    content: "As the man himself said, it was inevitable. \r\nOn Friday, Tesla CEO Elon Musk changed his Twitter bio to \"#bitcoin.\" This alone perhaps wouldn't be that interesting after all, Musk expressed his inter… [+2792 chars]"
+                },
+                {
+                    source: {
+                        "id": null,
+                        "name": "Gizmodo.com"
+                    },
+                    author: "Matt Novak",
+                    title: "Biden Pick for Treasury Dept Role Worked as Advisor to Controversial Crypto Company Ripple",
+                    description: "President Joe Biden’s pick to be the new comptroller of the currency at the U.S. Treasury Department, Michael S. Barr, used to be an advisor to the controversial cryptocurrency company Ripple, according to a new report from the Wall Street Journal.Read more...",
+                    url: "https://gizmodo.com/biden-pick-for-treasury-dept-role-worked-as-advisor-to-1846098749",
+                    urlToImage: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/bpeqqcnsfoj5qbizyfyz.jpg",
+                    publishedAt: "2021-01-21T11:30:00Z",
+                    content: "President Joe Bidens pick to be the new comptroller of the currency at the U.S. Treasury Department, Michael S. Barr, used to be an advisor to the controversial cryptocurrency company Ripple, accordi… [+2949 chars]"
+                },
+                {
+                    source: {
+                        "id": "techcrunch",
+                        "name": "TechCrunch"
+                    },
+                    author: "Connie Loizos",
+                    title: "Coinbase commits to a “better customer experience” following complaints",
+                    description: "Coinbase has a problem. As interest in bitcoin has soared along with its price, the popular cryptocurrency exchange has found itself the target of a growing spate of angry customers who haven’t been able to access customer service. A quick look at Twitter tel…",
+                    url: "http://techcrunch.com/2021/01/15/coinbase-commits-to-a-better-customer-experience-following-complaints/",
+                    urlToImage: "https://techcrunch.com/wp-content/uploads/2020/07/GettyImages-887657568.jpg?w=600",
+                    publishedAt: "2021-01-15T21:06:19Z",
+                    content: "Coinbase has a problem. As interest in bitcoin has soared along with its price, the popular cryptocurrency exchange has found itself the target of a growing spate of angry customers who haven’t been … [+3566 chars]"
+                },
+                {
+                    source: {
+                        "id": "techcrunch",
+                        "name": "TechCrunch"
+                    },
+                    author: "Lucas Matney",
+                    title: "Could meme stocks like GameStop kill bitcoin’s rise?",
+                    description: "Cryptocurrencies, more so than most other things, are only valuable because of a shared agreement that they are valuable. Their value is a product of digital handshakes over millions of transactions firming up that consensus. For bitcoin, the trust that it ha…",
+                    url: "http://techcrunch.com/2021/01/27/could-meme-stocks-like-gamestop-kill-bitcoins-rise/",
+                    urlToImage: "https://techcrunch.com/wp-content/uploads/2017/08/bitcoin-split-2017a.jpg?w=711",
+                    publishedAt: "2021-01-27T19:24:32Z",
+                    content: "Cryptocurrencies, more so than most other things, are only valuable because of a shared agreement that they are valuable. Their value is a product of digital handshakes over millions of transactions … [+3710 chars]"
+                },
+                {
+                    source: {
+                        "id": "techcrunch",
+                        "name": "TechCrunch"
+                    },
+                    author: "Lucas Matney",
+                    title: "Coinbase is going public via direct listing",
+                    description: "Coinbase plans to go public by way of a direct listing, the company announced in a blog post today. The cryptocurrency exchange was founded in 2012 and allows users to buy and trade decentralized tokens like bitcoin and ethereum. The company has raised over $…",
+                    url: "http://techcrunch.com/2021/01/28/coinbase-is-going-public-via-direct-listing/",
+                    urlToImage: "https://techcrunch.com/wp-content/uploads/2019/01/coinbase.jpg?w=750",
+                    publishedAt: "2021-01-28T19:36:35Z",
+                    content: "Coinbase plans to go public by way of a direct listing, the company announced in a blog post today.\r\nThe cryptocurrency exchange was founded in 2012 and allows users to buy and trade decentralized to… [+1258 chars]"
+                },
+                {
+                    source: {
+                        "id": null,
+                        "name": "Seeking Alpha"
+                    },
+                    author: null,
+                    title: "Bitcoin blasts through $30K - Seeking Alpha",
+                    description: "<ol><li>Bitcoin blasts through $30K  Seeking Alpha\r\n</li><li>Bitcoin Hits $31K for First Time, an Hour After Tearing Through $30K  Yahoo Finance\r\n</li><li>Bitcoin Value Leaps Over the $31K Handle, BTC Sees an All-Time Price High in 2021 | Market Updates  Bitc…",
+                    url: "https://seekingalpha.com/news/3648183-bitcoin-blasts-through-30k",
+                    urlToImage: "https://static3.seekingalpha.com/assets/og_image_192-59bfd51c9fe6af025b2f9f96c807e46f8e2f06c5ae787b15bf1423e6c676d4db.png",
+                    "publishedAt": "2021-01-02T12:59:00Z",
+                    "content": null
+                },
+                {
+                    source: {
+                        "id": null,
+                        "name": "Slashdot.org"
+                    },
+                    author: "EditorDavid",
+                    title: "Bitcoin Surges 25% In One Week. Warren Buffett Still Won't Buy It",
+                    description: "Last Sunday we reported Bitcoin's price had surged 50% in the previous month. \n\nIn the week since it's surged another 24.8%. \n\nAs Bitcoin celebrates its 12th anniversary, a Forbes columnist writes that Bitcoin \"soared to $34,000 yesterday — but here's why War…",
+                    "url": "https://news.slashdot.org/story/21/01/03/1844241/bitcoin-surges-25-in-one-week-warren-buffett-still-wont-buy-it",
+                    urlToImage: "https://a.fsdn.com/sd/topics/bitcoin_64.png",
+                    publishedAt: "2021-01-03T18:45:00Z",
+                    content: "Last Sunday we reported Bitcoin's price had surged 50% in the previous month.In the week since it's surged another 24.8%.\r\nAs Bitcoin celebrates its 12th anniversary, a Forbes columnist writes that B… [+1428 chars]"
+                }
+            ]
         }
     },
     methods: {
-        getNews() {
-            axios.get('/v2/everything?q=bitcoin&apiKey=9ea7329bd4964071a04d0f095746f02b').then(response => {
-                console.log(response.data.articles);
-                //this.news = response.data.data;
+        toggleCard(value) {
+            if(value == 'card') {
+                this.isCard = false
 
-            }).catch(error => {
-                console.error(error);
-            });
-
-
+            } else {
+                this.isCard = true
+            }
         },
+
+        // getNews() {
+        //     axios.get('/v2/everything?q=bitcoin&apiKey=9ea7329bd4964071a04d0f095746f02b').then(response => {
+        //         console.log(response.data.articles);
+        //         //this.news = response.data.data;
+
+        //     }).catch(error => {
+        //         console.error(error);
+        //     });
+
+
+        // },
     },
     mounted() {
-       // this.getNews()
+        // this.getNews()
     },
 }
 </script>
+
+<style scoped>
+    .card-toggle {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .list-toggle {
+       display: block;
+    }
+
+    .card-img {
+        display: block;
+    }
+
+     .list-img {
+        display: none;
+    }
+
+    .card-width {
+        width: 15rem;
+    }
+</style>
